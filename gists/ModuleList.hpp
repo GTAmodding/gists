0255e1248e5f8e0defe16a9967247df3d2c5ece4
Silent <zdanio95@gmail.com>
2018-04-08 14:24:47 +0200

    Updated ModuleList

--- gists/ModuleList.hpp
+++ gists/ModuleList.hpp
@@ -51,6 +51,10 @@ public:
 								EnumerateInternal( modules, cbNeeded / sizeof(HMODULE) );
 							}
 						}
+						else
+						{
+							EnumerateInternal( modules, INITIAL_SIZE / sizeof(HMODULE) );
+						}
 					}
 					else
 					{
@@ -144,7 +148,7 @@ private:
 					const wchar_t* dotPos = wcsrchr( nameBegin, '.' );
 					if ( dotPos != nullptr )
 					{
-						m_moduleList.emplace_back( *modules, std::wstring( nameBegin, std::distance( nameBegin, dotPos ) ) );
+						m_moduleList.emplace_back( *modules, std::wstring( nameBegin, dotPos ) );
 					}
 					else
 					{
