5cb87b684c16269f28142623ede26832263aef10
Silent <zdanio95@gmail.com>
2018-06-05 01:19:44 +0200

    Update MemoryMgr

--- gists/MemoryMgr.h
+++ gists/MemoryMgr.h
@@ -20,6 +20,7 @@
 #include <Windows.h>
 
 #include <cstdint>
+#include <cassert>
 
 #ifndef _MEMORY_NO_CRT
 #include <initializer_list>
@@ -32,394 +33,11 @@ enum
 	PATCH_JUMP
 };
 
-inline signed char* GetVer()
-{
-	static signed char	bVer = -1;
-	return &bVer;
-}
-
-inline bool* GetEuropean()
-{
-	static bool			bEuropean;
-	return &bEuropean;
-}
-
-inline void* GetDummy()
-{
-	static uintptr_t		dwDummy;
-	return &dwDummy;
-}
-
 template<typename AT>
 inline AT DynBaseAddress(AT address)
 {
 	return (ptrdiff_t)GetModuleHandle(nullptr) - 0x400000 + address;
 }
-
-#if defined _GTA_III
-
-inline void InitializeVersions()
-{
-	signed char*	bVer = GetVer();
-
-	if ( *bVer == -1 )
-	{
-		if (*(uint32_t*)0x5C1E70 == 0x53E58955) *bVer = 0;
-		else if (*(uint32_t*)0x5C2130 == 0x53E58955) *bVer = 1;
-		else if (*(uint32_t*)0x5C6FD0 == 0x53E58955) *bVer = 2;
-	}
-}
-
-// This function initially detects III version then chooses the address basing on game version
-template<typename T>
-inline T AddressByVersion(uintptr_t address10, uintptr_t address11, uintptr_t addressSteam)
-{
-	InitializeVersions();
-
-	signed char		bVer = *GetVer();
-
-	switch ( bVer )
-	{
-	case 1:
-#ifdef assert
-		assert(address11);
-#endif
-		return (T)address11;
-	case 2:
-#ifdef assert
-		assert(addressSteam);
-#endif
-		return (T)addressSteam;
-	default:
-#ifdef assert
-		assert(address10);
-#endif
-		return (T)address10;
-	}
-}
-
-#elif defined _GTA_VC
-
-inline void InitializeVersions()
-{
-	signed char*	bVer = GetVer();
-
-	if ( *bVer == -1 )
-	{
-		if (*(uint32_t*)0x667BF0 == 0x53E58955) *bVer = 0;
-		else if (*(uint32_t*)0x667C40 == 0x53E58955) *bVer = 1;
-		else if (*(uint32_t*)0x666BA0 == 0x53E58955) *bVer = 2;
-	}
-}
-
-// This function initially detects VC version then chooses the address basing on game version
-template<typename T>
-inline T AddressByVersion(uintptr_t address10, uintptr_t address11, uintptr_t addressSteam)
-{
-	InitializeVersions();
-
-	signed char	bVer = *GetVer();
-
-	switch ( bVer )
-	{
-	case 1:
-#ifdef assert
-		assert(address11);
-#endif
-		return (T)address11;
-	case 2:
-#ifdef assert
-		assert(addressSteam);
-#endif
-		return (T)addressSteam;
-	default:
-#ifdef assert
-		assert(address10);
-#endif
-		return (T)address10;
-	}
-}
-
-#elif defined _GTA_SA
-
-inline void InitializeVersions()
-{
-	signed char*	bVer = GetVer();
-	bool*			bEuropean = GetEuropean();
-
-	if ( *bVer == -1 )
-	{
-		if ( *(uint32_t*)DynBaseAddress(0x82457C) == 0x94BF )
-		{
-			// 1.0 US
-			*bVer = 0;
-			*bEuropean = false;
-		}
-		else if ( *(uint32_t*)DynBaseAddress(0x8245BC) == 0x94BF )
-		{
-			// 1.0 EU
-			*bVer = 0;
-			*bEuropean = true;
-		}
-		else if ( *(uint32_t*)DynBaseAddress(0x8252FC) == 0x94BF )
-		{
-			// 1.01 US
-			*bVer = 1;
-			*bEuropean = false;
-		}
-		else if ( *(uint32_t*)DynBaseAddress(0x82533C) == 0x94BF )
-		{
-			// 1.01 EU
-			*bVer = 1;
-			*bEuropean = true;
-		}
-		else if (*(uint32_t*)DynBaseAddress(0x85EC4A) == 0x94BF )
-		{
-			// 3.0
-			*bVer = 2;
-			*bEuropean = false;
-		}
-
-		else if ( *(uint32_t*)DynBaseAddress(0x858D21) == 0x3539F633 )
-		{
-			// newsteam r1
-			*bVer = 3;
-			*bEuropean = false;
-		}
-		else if ( *(uint32_t*)DynBaseAddress(0x858D51) == 0x3539F633 )
-		{
-			// newsteam r2
-			*bVer = 4;
-			*bEuropean = false;
-		}
-		else if ( *(uint32_t*)DynBaseAddress(0x858C61) == 0x3539F633 )
-		{
-			// newsteam r2 lv
-			*bVer = 5;
-			*bEuropean = false;
-		}
-	}
-}
-
-inline void InitializeRegion_10()
-{
-	bool*			bEuropean = GetEuropean();
-	signed char*	bVer = GetVer();
-
-	if ( *bVer == -1 )
-	{
-		if ( *(uint32_t*)0x82457C == 0x94BF )
-		{
-			*bVer = 0;
-			*bEuropean = false;
-		}
-		else if ( *(uint32_t*)0x8245BC == 0x94BF )
-		{
-			*bVer = 0;
-			*bEuropean = true;
-		}
-		else
-		{
-#ifdef assert
-			assert(!"AddressByRegion_10 on non-1.0 EXE!");
-#endif
-		}
-	}
-}
-
-inline void InitializeRegion_11()
-{
-	bool*			bEuropean = GetEuropean();
-	signed char*	bVer = GetVer();
-
-	if ( *bVer == -1 )
-	{
-		if ( *(uint32_t*)0x8252FC == 0x94BF )
-		{
-			*bVer = 1;
-			*bEuropean = false;
-		}
-		else if ( *(uint32_t*)0x82533C == 0x94BF )
-		{
-			*bVer = 1;
-			*bEuropean = true;
-		}
-		else
-		{
-#ifdef assert
-			assert(!"AddressByRegion_11 on non-1.01 EXE!");
-#endif
-		}
-	}
-}
-
-// This function initially detects SA version then chooses the address basing on game version
-template<typename T>
-inline T AddressByVersion(uintptr_t address10, uintptr_t address11, uintptr_t addressSteam)
-{
-	InitializeVersions();
-
-	signed char	bVer = *GetVer();
-	bool			bEuropean = *GetEuropean();
-
-	switch ( bVer )
-	{
-	case 1:
-#ifdef assert
-		assert(address11);
-#endif
-
-		// Safety measures - if null, return dummy var pointer to prevent a crash
-		if ( !address11 )
-			return (T)GetDummy();
-
-		// Adjust to US if needed
-		if ( !bEuropean && address11 > 0x746FA0 )
-		{
-			if ( address11 < 0x7BB240 )
-				address11 -= 0x50;
-			else
-				address11 -= 0x40;
-		}
-		return (T)address11;
-	case 2:
-#ifdef assert
-		assert(addressSteam);
-#endif
-		// Safety measures - if null, return dummy var pointer to prevent a crash
-		if ( !addressSteam )
-			return (T)GetDummy();
-
-		return (T)addressSteam;
-	case 3:
-	case 4:
-	case 5:
-		// TODO: DO
-		return (T)GetDummy();
-	default:
-#ifdef assert
-		assert(address10);
-#endif
-		// Adjust to EU if needed
-		if ( bEuropean && address10 > 0x7466D0 )
-		{
-			if ( address10 < 0x7BA940 )
-				address10 += 0x50;
-			else
-				address10 += 0x40;
-		}
-		return (T)address10;
-	}
-}
-
-template<typename T>
-inline T AddressByVersion(uintptr_t address10, uintptr_t address11, uintptr_t addressSteam, uintptr_t addressNewsteamR2, uintptr_t addressNewsteamR2_LV)
-{
-	InitializeVersions();
-
-	signed char	bVer = *GetVer();
-	bool			bEuropean = *GetEuropean();
-
-	switch ( bVer )
-	{
-	case 1:
-#ifdef assert
-		assert(address11);
-#endif
-
-		// Safety measures - if null, return dummy var pointer to prevent a crash
-		if ( !address11 )
-			return (T)GetDummy();
-
-		// Adjust to US if needed
-		if ( bEuropean && address11 > 0x746FA0 )
-		{
-			if ( address11 < 0x7BB240 )
-				address11 -= 0x50;
-			else
-				address11 -= 0x40;
-		}
-		return (T)address11;
-	case 2:
-#ifdef assert
-		assert(addressSteam);
-#endif
-		// Safety measures - if null, return dummy var pointer to prevent a crash
-		if ( !addressSteam )
-			return (T)GetDummy();
-
-		return (T)addressSteam;
-	case 3:
-		return (T)GetDummy();
-	case 4:
-#ifdef assert
-		assert(addressNewsteamR2);
-#endif
-		if ( !addressNewsteamR2 )
-			return (T)GetDummy();
-
-		return (T)DynBaseAddress(addressNewsteamR2);
-	case 5:
-#ifdef assert
-		assert(addressNewsteamR2_LV);
-#endif
-		if ( !addressNewsteamR2_LV )
-			return (T)GetDummy();
-
-		return (T)DynBaseAddress(addressNewsteamR2_LV);
-	default:
-#ifdef assert
-		assert(address10);
-#endif
-		// Adjust to EU if needed
-		if ( bEuropean && address10 > 0x7466D0 )
-		{
-			if ( address10 < 0x7BA940 )
-				address10 += 0x50;
-			else
-				address10 += 0x40;
-		}
-		return (T)address10;
-	}
-}
-
-template<typename T>
-inline T AddressByRegion_10(uintptr_t address10)
-{
-	InitializeRegion_10();
-
-	bool			bEuropean = *GetEuropean();
-
-	// Adjust to EU if needed
-	if ( bEuropean && address10 > 0x7466D0 )
-	{
-		if ( address10 < 0x7BA940 )
-			address10 += 0x50;
-		else
-			address10 += 0x40;
-	}
-	return (T)address10;
-}
-
-template<typename T>
-inline T AddressByRegion_11(uintptr_t address11)
-{
-	InitializeRegion_11();
-
-	bool			bEuropean = *GetEuropean();
-
-	// Adjust to US if needed
-	if ( !bEuropean && address11 > 0x746FA0 )
-	{
-		if ( address11 < 0x7BB240 )
-			address11 -= 0x50;
-		else
-			address11 -= 0x40;
-	}
-	return (T)address11;
-}
-
-#endif
 
 namespace Memory
 {
@@ -431,8 +49,8 @@ namespace Memory
 	template<typename AT>
 	inline void		Patch(AT address, std::initializer_list<uint8_t> list )
 	{
-		uint8_t* const addr = (uint8_t*)address;
-		std::copy( list.begin(), list.end(), stdext::checked_array_iterator<uint8_t*>(addr, list.size()) );
+		uint8_t* addr = reinterpret_cast<uint8_t*>(address);
+		std::copy( list.begin(), list.end(), stdext::make_checked_array_iterator(addr, list.size()) );
 	}
 #endif
 
@@ -446,38 +64,62 @@ namespace Memory
 	} while ( --count != 0 ); }
 #endif
 
-	template<typename AT, typename HT>
-	inline void		InjectHook(AT address, HT hook)
+	template<typename AT, typename Func>
+	inline void		InjectHook(AT address, Func hook)
 	{
-		intptr_t		dwHook;
-		_asm
+		union member_cast
 		{
-			mov		eax, hook
-			mov		dwHook, eax
-		}
+			intptr_t addr;
+			Func funcPtr;
+		} cast;
+		static_assert( sizeof(cast.addr) == sizeof(cast.funcPtr), "member_cast failure!" );
+		cast.funcPtr = hook;
 
-		*(ptrdiff_t*)((intptr_t)address + 1) = dwHook - (intptr_t)address - 5;
+		*(int32_t*)((intptr_t)address + 1) = static_cast<int32_t>(cast.addr - (intptr_t)address - 5);
 	}
 
-	template<typename AT, typename HT>
-	inline void		InjectHook(AT address, HT hook, unsigned int nType)
+	template<typename AT, typename Func>
+	inline void		InjectHook(AT address, Func hook, unsigned int nType)
 	{
-		intptr_t		dwHook;
-		_asm
-		{
-			mov		eax, hook
-			mov		dwHook, eax
-		}
-
 		*(uint8_t*)address = nType == PATCH_JUMP ? 0xE9 : 0xE8;
-
-		*(ptrdiff_t*)((intptr_t)address + 1) = dwHook - (intptr_t)address - 5;
+		InjectHook(address, hook);
 	}
 
 	template<typename Func, typename AT>
 	inline void		ReadCall(AT address, Func& func)
 	{
-		func = Func(*(ptrdiff_t*)((intptr_t)address+1) + (intptr_t)address + 5);
+		union member_cast
+		{
+			intptr_t addr;
+			Func funcPtr;
+		} cast;
+		static_assert( sizeof(cast.addr) == sizeof(cast.funcPtr), "member_cast failure!" );
+
+		cast.addr = (intptr_t)address + 5 + *(int32_t*)((intptr_t)address+1);
+		func = cast.funcPtr;
+	}
+
+	template<typename AT>
+	inline void*	ReadCallFrom(AT address, ptrdiff_t offset = 0)
+	{
+		uintptr_t addr;
+		ReadCall( address, addr );
+		return reinterpret_cast<void*>( addr + offset );
+	}
+
+#ifndef _MEMORY_NO_CRT
+	inline bool MemEquals(uintptr_t address, std::initializer_list<uint8_t> val)
+	{
+		const uint8_t* mem = reinterpret_cast<const uint8_t*>(address);
+		return std::equal( val.begin(), val.end(), stdext::make_checked_array_iterator(mem, val.size()) );
+	}
+#endif
+
+	template<typename AT>
+	inline AT Verify(AT address, uintptr_t expected)
+	{
+		assert( uintptr_t(address) == expected );
+		return address;
 	}
 
 	namespace DynBase
@@ -519,6 +161,25 @@ namespace Memory
 		{
 			Memory::ReadCall(DynBaseAddress(address), func);
 		}
+
+		template<typename AT>
+		inline void*	ReadCallFrom(AT address, ptrdiff_t offset = 0)
+		{
+			return Memory::ReadCallFrom(DynBaseAddress(address), offset);
+		}
+
+#ifndef _MEMORY_NO_CRT
+		inline bool MemEquals(uintptr_t address, std::initializer_list<uint8_t> val)
+		{
+			return Memory::MemEquals(DynBaseAddress(address), std::move(val));
+		}
+
+		template<typename AT>
+		inline AT Verify(AT address, uintptr_t expected)
+		{
+			return Memory::Verify(address, DynBaseAddress(expected));
+		}
+#endif
 	};
 
 	namespace VP
@@ -557,9 +218,9 @@ namespace Memory
 		{
 			DWORD		dwProtect[2];
 
-			VirtualProtect((void*)((DWORD)address + 1), 4, PAGE_EXECUTE_READWRITE, &dwProtect[0]);
+			VirtualProtect((void*)((DWORD_PTR)address + 1), 4, PAGE_EXECUTE_READWRITE, &dwProtect[0]);
 			Memory::InjectHook( address, hook );
-			VirtualProtect((void*)((DWORD)address + 1), 4, dwProtect[0], &dwProtect[1]);
+			VirtualProtect((void*)((DWORD_PTR)address + 1), 4, dwProtect[0], &dwProtect[1]);
 		}
 
 		template<typename AT, typename HT>
@@ -576,6 +237,25 @@ namespace Memory
 		inline void		ReadCall(AT address, Func& func)
 		{
 			Memory::ReadCall(address, func);
+		}
+
+		template<typename AT>
+		inline void*	ReadCallFrom(AT address, ptrdiff_t offset = 0)
+		{
+			return Memory::ReadCallFrom(address, offset);
+		}
+
+#ifndef _MEMORY_NO_CRT
+		inline bool MemEquals(uintptr_t address, std::initializer_list<uint8_t> val)
+		{
+			return Memory::MemEquals(address, std::move(val));
+		}
+#endif
+
+		template<typename AT>
+		inline AT Verify(AT address, uintptr_t expected)
+		{
+			return Memory::Verify(address, expected);
 		}
 
 		namespace DynBase
@@ -617,6 +297,26 @@ namespace Memory
 			{
 				Memory::ReadCall(DynBaseAddress(address), func);
 			}
+
+			template<typename AT>
+			inline void*	ReadCallFrom(AT address, ptrdiff_t offset = 0)
+			{
+				Memory::ReadCallFrom(DynBaseAddress(address), offset);
+			}
+
+#ifndef _MEMORY_NO_CRT
+			inline bool MemEquals(uintptr_t address, std::initializer_list<uint8_t> val)
+			{
+				return Memory::MemEquals(DynBaseAddress(address), std::move(val));
+			}
+#endif
+
+			template<typename AT>
+			inline AT Verify(AT address, uintptr_t expected)
+			{
+				return Memory::Verify(address, DynBaseAddress(expected));
+			}
+
 		};
 	};
 };
